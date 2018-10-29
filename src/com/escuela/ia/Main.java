503b4e61dcef106136d21524e01109f0337d880e
Jessi <jessicalume@gmail.com>
2018-10-29 12:36:28 -0300

    Libs

--- src/com/escuela/ia/Main.java
+++ src/com/escuela/ia/Main.java
@@ -1,9 +1,14 @@
 package com.escuela.ia;
 
+import Hibernate.HibernateUtil;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	HibernateUtil.getSessionFactory();
+
 
 	}
 
