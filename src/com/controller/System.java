0ac321a8f8b154da5e9f0297384b121e667038b7
Jessi <jessicalume@gmail.com>
2018-10-29 11:31:04 -0300

    Prueba jessi
    systema

--- src/com/controller/System.java
+++ src/com/controller/System.java
@@ -2,7 +2,16 @@ package com.controller;
 
 import java.util.List;
 
+import bean.Cliente;
+import bean.Servicio;
+import bean.Venta;
+
 public class System {
+	
+	private List<Alumno> alumnos;
+	private List<Titular> titulares;
+	private List<Adicional> adicionales;
+	private Escolaridad escolaridad;
 	
 	public void crearAlumno(int legajo, String nombre) {
 		
